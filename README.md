a08fc3a84eeed07f5ca237a949b1a9daa75b039b
Taco Kemna <siriousje@gmail.com>
2018-02-02 10:32:17 +1100

    would be nice if visual code could warn that you didn't save files before pushing :-P

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # jenkins
 some useful jenkins scripts
+
+## clean-builds-and-workspaces.groovy
+
+this one just cleans a single node manually.
--- clean-builds-and-workspaces.groovy
+++ clean-builds-and-workspaces.groovy
@@ -23,7 +23,7 @@ def cleanWorkspace (items) {
       for (subitem in item.items) {
         if (subitem.class.canonicalName == "org.jenkinsci.plugins.workflow.job.WorkflowJob") {
           println("    $subitem.fullName is a workflow job");
-          processBuildsForWorkflowItem(subitem, true);
+          processBuildsForWorkflowItem(subitem, true); // get rid of all builds for all branches
         }
         else {
           println("    unknown subitem: $subitem");
@@ -31,44 +31,42 @@ def cleanWorkspace (items) {
       }        
     }
     else if (item.isBuilding()) { 
-       println("  skipping job $item.name, currently building")
+      println("  skipping job $item.name, currently building")
     }
     else if (item.class.canonicalName == "org.jenkinsci.plugins.workflow.job.WorkflowJob") {
-      	println("  $item.name is a workflow job, cleaning builds");
-        processBuildsForWorkflowItem(item, false); // keep young builds
+      println("  $item.name is a workflow job, cleaning builds");
+      processBuildsForWorkflowItem(item, false); // keep young builds
     } 
-    else if (item.class.canonicalName != "hudson.model.ExternalJob") {
-      	println("Processing item " + item.name + " (" + item.class.canonicalName+")")
-        println("  Wiping out workspace of job "+item.name)
-        item.doDoWipeOutWorkspace()
+    else if (item.class.canonicalName != "hudson.model.ExternalJob") { // anything else should have the wipeout
+      println("wiping workspace for item $item.name " + " (" + item.class.canonicalName+")")
+      item.doDoWipeOutWorkspace()
     } 
     else {
-        println("  Item of type "+item.class.canonicalName + " cannot have its workspace cleaned")
+        println("  Item of type " + item.class.canonicalName + " cannot have its workspace cleaned")
     }
- }
+  }
 }
 
 def processBuildsForWorkflowItem(item, deleteAllBranches) {
-  	if (item.class.canonicalName != "org.jenkinsci.plugins.workflow.job.WorkflowJob") {
-    	println("      this is not a WorkflowJob, cannot process builds")
-  	}
-  	else if (item.builds.isEmpty()) {
-      	println("      no builds to delete")
-  	}
-  	else {
-      	for (build in item.builds) {
-            if (build.isBuilding()) {
-              	println("     won't be deleting build $build.number as it is currently building");
-            } 
-            else if (deleteAllBranches || isOldBuild(build)) {
-              	println("     found build $build.number done at $build.time, which is old and can be deleted")
-              	build.deleteArtifacts()
-              	build.delete()
-
-          	}
-          	else {
-              	println("     found build $build.number done at $build.time, which is too young to be deleted")
-          	}
-      	}
-  	}
+  if (item.class.canonicalName != "org.jenkinsci.plugins.workflow.job.WorkflowJob") {
+    println("      this is not a WorkflowJob, cannot process builds")
+  }
+  else if (item.builds.isEmpty()) {
+    println("      no builds to delete")
+  }
+  else {
+    for (build in item.builds) {
+      if (build.isBuilding()) {
+        println("     won't be deleting build $build.number as it is currently building");
+      } 
+      else if (deleteAllBranches || isOldBuild(build)) {
+        println("     found build $build.number done at $build.time, which is old and can be deleted")
+        build.deleteArtifacts() // probably also happens on build.delete, but perhaps later we just want the artifacts wiped
+        build.delete()
+      }
+      else {
+        println("     found build $build.number done at $build.time, which is too young to be deleted")
+      }
+    }
+  }
 }
